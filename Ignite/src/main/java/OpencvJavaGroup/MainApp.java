6a605c96d2d1c302e5a163fe37d83de2b570b073
damian <damiakuz@wp.pl>
2019-04-29 22:47:46 +0200

    add: launch ignite with configuration

---
+++ Ignite/src/main/java/OpencvJavaGroup/MainApp.java
@@ -0,0 +1,59 @@
+package OpencvJavaGroup;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        MainApp app = new MainApp();
+        app.igniteInstance(app.igniteConfiguration());
+        System.out.println("hello world");
+    }
+    @Bean
+    public IgniteConfiguration igniteConfiguration() {
+        IgniteConfiguration igniteConfiguration = new IgniteConfiguration();
+        igniteConfiguration.setPeerClassLoadingEnabled(false);
+
+        igniteConfiguration.setIncludeEventTypes(
+                org.apache.ignite.events.EventType.EVT_TASK_STARTED,
+                org.apache.ignite.events.EventType.EVT_TASK_FINISHED,
+                org.apache.ignite.events.EventType.EVT_TASK_FAILED,
+                org.apache.ignite.events.EventType.EVT_TASK_TIMEDOUT,
+                org.apache.ignite.events.EventType.EVT_TASK_SESSION_ATTR_SET,
+                org.apache.ignite.events.EventType.EVT_TASK_REDUCED,
+                org.apache.ignite.events.EventType.EVT_CACHE_OBJECT_PUT,
+                org.apache.ignite.events.EventType.EVT_CACHE_OBJECT_READ,
+                org.apache.ignite.events.EventType.EVT_CACHE_OBJECT_REMOVED);
+
+        TcpDiscoveryMulticastIpFinder tcpDiscoveryMulticastIpFinder = new TcpDiscoveryMulticastIpFinder();
+        tcpDiscoveryMulticastIpFinder.setAddresses(Collections.singletonList("127.0.0.1:47500..47509"));
+        TcpDiscoverySpi tcpDiscoverySpi = new TcpDiscoverySpi();
+        tcpDiscoverySpi.setIpFinder(tcpDiscoveryMulticastIpFinder);
+        igniteConfiguration.setDiscoverySpi(tcpDiscoverySpi);
+
+        CacheConfiguration cacheConfiguration = new CacheConfiguration();
+        cacheConfiguration.setName("session-cache");
+        cacheConfiguration.setCacheMode(CacheMode.REPLICATED);
+
+        igniteConfiguration.setCacheConfiguration(cacheConfiguration);
+
+        return igniteConfiguration;
+    }
+
+    @Bean("igniteInstance")
+    public Ignite igniteInstance(IgniteConfiguration igniteConfiguration) {
+        Ignite start = Ignition.start(igniteConfiguration);
+        start.active(true);
+        return start;
+    }
+}
+
